5f9bdbd28b2c6ea1a675cbbd5677a468ae94454f
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 16:30:19 +0800

    reset

--- mlink/index.js
+++ mlink/index.js
@@ -1,10 +1,8 @@
 var modifiedResponse = $response.body;
 var responseJson = JSON.parse(modifiedResponse);
 
-responseJson.data.vipType = "free";
-responseJson.data.level = 1;
-responseJson.data.expireTime = 1703578727384;
-responseJson.data.role = "anonymous";
+responseJson.data.vipType = "svip";
+responseJson.data.level = 3;
 
 modifiedResponse = JSON.stringify(responseJson);
 $done({ body: modifiedResponse });
