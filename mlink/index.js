f49ac1b33b8716729bbd699761eb310cfaba2da5
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 16:15:48 +0800

    freeTime

--- mlink/index.js
+++ mlink/index.js
@@ -3,6 +3,7 @@ var responseJson = JSON.parse(modifiedResponse);
 
 responseJson.data.vipType = "svip";
 responseJson.data.level = 3;
+responseJson.data.freeTime = 3369600000;
 
 modifiedResponse = JSON.stringify(responseJson);
 $done({ body: modifiedResponse });
