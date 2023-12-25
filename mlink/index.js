acae8334291493f4a5d22392b3e1e23686dd4382
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 16:24:55 +0800

    anonymous

--- mlink/index.js
+++ mlink/index.js
@@ -1,9 +1,10 @@
 var modifiedResponse = $response.body;
 var responseJson = JSON.parse(modifiedResponse);
 
-responseJson.data.vipType = "svip";
+responseJson.data.vipType = "free";
 responseJson.data.level = 3;
-responseJson.data.freeTime = 3369600000;
+responseJson.data.expireTime = 1703578727384;
+responseJson.data.role = "anonymous";
 
 modifiedResponse = JSON.stringify(responseJson);
 $done({ body: modifiedResponse });
