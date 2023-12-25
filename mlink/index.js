06feb4fce510b549f680bc065c62cafa96a975d6
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 14:07:40 +0800

    first commit

---
+++ mlink/index.js
@@ -0,0 +1,6 @@
+var modifiedResponse = $response.body;
+var responseJson = JSON.parse(modifiedResponse);
+responseJson.data.vipType = "svip";
+responseJson.data.level = 3;
+modifiedResponse = JSON.stringify(responseJson);
+$done({ body: modifiedResponse });
