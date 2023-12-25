beb2b0cf5710fa8cf99d95e253c3328076540ff6
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 16:26:09 +0800

    level

--- mlink/index.js
+++ mlink/index.js
@@ -2,7 +2,7 @@ var modifiedResponse = $response.body;
 var responseJson = JSON.parse(modifiedResponse);
 
 responseJson.data.vipType = "free";
-responseJson.data.level = 3;
+responseJson.data.level = 1;
 responseJson.data.expireTime = 1703578727384;
 responseJson.data.role = "anonymous";
 
