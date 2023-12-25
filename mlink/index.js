66930735d63bae372903a99c25ad8e1a31c7e984
lianghui.an <lianghui.an@servision.com.cn>
2023-12-25 14:30:08 +0800

    expireTime

--- mlink/index.js
+++ mlink/index.js
@@ -1,6 +1,9 @@
 var modifiedResponse = $response.body;
 var responseJson = JSON.parse(modifiedResponse);
+
 responseJson.data.vipType = "svip";
 responseJson.data.level = 3;
+responseJson.data.expireTime = 1706283100000;
+
 modifiedResponse = JSON.stringify(responseJson);
 $done({ body: modifiedResponse });
